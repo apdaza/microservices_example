eb9fb2439cd17ed781c6385278f00aa73394bc66
Javier Melo <javier.hospital@bodytechcorp.com>
2021-11-11 23:00:39 -0500

    adding sql insert scheme for carrito

--- apicrud/database/tienda.sql
+++ apicrud/database/tienda.sql
@@ -40,3 +40,10 @@ INSERT INTO producto (product_id, producto_nombre, producto_descripcion, product
 VALUES (1, 'Arroz', 'Descripcion arroz', 3500, 10),
 (2, 'Aceite', 'Descripcion aceite', 4500, 20),
 (3, 'Leche', 'Descripcion leche', 5500, 30);
+
+
+INSERT INTO carrito (carrito_id, status_carrito, date_carrito, customer_id_fk)
+VALUES (1, 'Creado', '2021-04-18 23:56:40', 2),
+(2, 'En Creación', '2021-04-16 02:56:40', 3),
+(3, 'Error', '2021-04-16 09:56:40', 3),
+(3, 'Finalizado', '2021-04-14 05:56:40', 1),
